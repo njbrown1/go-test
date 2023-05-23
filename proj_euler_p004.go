b7e747157cec92adf2d756427a16fc4a01b69898
Nathan Brown <nathanjeffreybrown@gmail.com>
2023-05-23 12:23:31 -0500

    fixed if statement for 6-digit numbers

--- proj_euler_p004.go
+++ proj_euler_p004.go
@@ -15,22 +15,22 @@ func main() {
 	fmt.Println("a initial:", a, "\nb initial:", b)
 	fmt.Println("a * b initial:", a*b)
 
-	for a <= 303 {
-		for b <= 303 {
+	for a <= 330 {
+		for b <= 330 {
+
 			n = a * b
-
 			nString = strconv.Itoa(n)
-			fmt.Printf("a: %d, b: %d, n: %d, length: %d, nString: %s\n", a, b, n, len(nString), nString) // ensuring that the 'for' loop works correctly
-
 			b++
 
 			if n > 99999 {
-				if nString[0] == nString[5] {
-					fmt.Println("kind of a palindrome")
+				if (nString[0] == nString[5] && nString[1] == nString[4]) && nString[2] == nString[3] {
+					fmt.Println("palindrome found!")
+					fmt.Printf("a: %d, b: %d, n: %d, length: %d, nString: %s\n", a, b, n, len(nString), nString) // ensuring that the 'for' loop works correctly
 				}
 			} else {
 				if nString[0] == nString[4] && nString[1] == nString[3] {
 					fmt.Println("palindrome found!")
+					fmt.Printf("a: %d, b: %d, n: %d, length: %d, nString: %s\n", a, b, n, len(nString), nString) // ensuring that the 'for' loop works correctly
 				}
 			}
 
