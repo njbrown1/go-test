e995dd74194f7f88193eedd229863ec90731e01f
Nathan Brown <nathanjeffreybrown@gmail.com>
2023-05-23 12:40:32 -0500

    completed the puzzle

--- proj_euler_p004.go
+++ proj_euler_p004.go
@@ -12,32 +12,33 @@ func main() {
 	largest_palindrome := 0
 	var nString string // defaults to an empty string
 
-	for a <= 300 {
-		for b <= 300 {
+	for a <= 999 {
+		for b <= 999 {
 
 			n = a * b
 			nString = strconv.Itoa(n)
 			b++
 
 			if n > 99999 { // if n has 6 digits
-				if (nString[0] == nString[5] && nString[1] == nString[4]) && nString[2] == nString[3] { // taking the nth element from the string and comparing it
-					fmt.Println("palindrome found!")
-					fmt.Printf("a: %d, b: %d, n: %d, length: %d, nString: %s\n", a, b, n, len(nString), nString) // ensuring that the 'for' loop works correctly
+
+				if (nString[0] == nString[5] && nString[1] == nString[4]) && nString[2] == nString[3] { // taking the nth element from the string and running comparisons
 
 					if n > largest_palindrome {
 						largest_palindrome = n
 						fmt.Println("new LARGEST palindrome found!")
+						fmt.Printf("a: %d, b: %d, n: %d, length: %d, nString: %s\n", a, b, n, len(nString), nString) // showing a, b, n, and the new largest palindrome
 					}
+
 				}
 
-			} else {
-				if nString[0] == nString[4] && nString[1] == nString[3] { // if n has 5 digits
-					fmt.Println("palindrome found!")
-					fmt.Printf("a: %d, b: %d, n: %d, length: %d, nString: %s\n", a, b, n, len(nString), nString) // ensuring that the 'for' loop works correctly
+			} else if n > 9999 { // if n has 5 digits
+
+				if nString[0] == nString[4] && nString[1] == nString[3] {
 
 					if n > largest_palindrome {
 						largest_palindrome = n
 						fmt.Println("new LARGEST palindrome found!")
+						fmt.Printf("a: %d, b: %d, n: %d, length: %d, nString: %s\n", a, b, n, len(nString), nString) // showing a, b, n, and the new largest palindrome
 					}
 				}
 
@@ -49,4 +50,7 @@ func main() {
 		a++
 
 	}
+
+	fmt.Println("The largest palindrome attainable by multiplying two 3-digit integers together is:", largest_palindrome)
+
 }
