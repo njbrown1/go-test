f0160ca3f19c1b336b24df215ddf856bb1cbf135
Nathan Brown <nathanjeffreybrown@gmail.com>
2023-05-22 19:23:04 -0500

    added rounding

--- proj_euler_p003.go
+++ proj_euler_p003.go
@@ -5,7 +5,7 @@ import "math"
 
 func main() {
 	var n int = 24 // where n is the composite number we are testing
-	var s = math.Sqrt(float64(n))
+	var s = math.Round(math.Sqrt(float64(n))) // where s is the square root of n, rounded up
 	fmt.Println(n)
 	fmt.Println(s)
 }
