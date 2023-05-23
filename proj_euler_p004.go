b31f501e27aa64514b94159769c52f07cce606bd
Nathan Brown <nathanjeffreybrown@gmail.com>
2023-05-23 11:46:19 -0500

    made the if statement considerably better

--- proj_euler_p004.go
+++ proj_euler_p004.go
@@ -3,28 +3,29 @@ package main
 import "fmt"
 
 func main() {
-	a := 100
-	b := 100
+	a := 300
+	b := 300
 	n := 0
+	// c := 0
 
 	fmt.Println("a initial:", a, "\nb initial:", b)
 	fmt.Println("a * b initial:", a*b)
 
-	for a <= 104 {
-		for b <= 104 {
+	for a <= 302 {
+		for b <= 302 {
 			n = a * b
 			fmt.Printf("a: %d, b: %d, n: %d \n", a, b, n) // ensuring that the 'for' loop works correctly
 			b++
 
-			if ((n) / (10 ^ 4)) >= 10 {
-				fmt.Println("5 digits long")
-			} else if ((n) / (10 ^ 4)) >= 1 {
+			if n > 99999 {
 				fmt.Println("6 digits long")
+			} else {
+				fmt.Println("5 digits long")
 			}
 
 		}
 
-		b = 100
+		b = 300
 		a++
 
 	}
