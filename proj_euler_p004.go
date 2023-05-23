44d807339ed0bb13a8d69b25809dcada627302a6
Nathan Brown <nathanjeffreybrown@gmail.com>
2023-05-23 12:44:43 -0500

    added helpful code comments

--- proj_euler_p004.go
+++ proj_euler_p004.go
@@ -16,14 +16,14 @@ func main() {
 		for b <= 999 {
 
 			n = a * b
-			nString = strconv.Itoa(n)
+			nString = strconv.Itoa(n) // convert n to a string, 'nString''
 			b++
 
 			if n > 99999 { // if n has 6 digits
 
 				if (nString[0] == nString[5] && nString[1] == nString[4]) && nString[2] == nString[3] { // taking the nth element from the string and running comparisons
 
-					if n > largest_palindrome {
+					if n > largest_palindrome { // if n is larger than the largest palindrome, make n the new largest palindrome
 						largest_palindrome = n
 						fmt.Println("new LARGEST palindrome found!")
 						fmt.Printf("a: %d, b: %d, n: %d, length: %d, nString: %s\n", a, b, n, len(nString), nString) // showing a, b, n, and the new largest palindrome
@@ -35,7 +35,7 @@ func main() {
 
 				if nString[0] == nString[4] && nString[1] == nString[3] {
 
-					if n > largest_palindrome {
+					if n > largest_palindrome { // if n is larger than the largest palindrome, make n the new largest palindrome
 						largest_palindrome = n
 						fmt.Println("new LARGEST palindrome found!")
 						fmt.Printf("a: %d, b: %d, n: %d, length: %d, nString: %s\n", a, b, n, len(nString), nString) // showing a, b, n, and the new largest palindrome
@@ -52,5 +52,5 @@ func main() {
 	}
 
 	fmt.Println("The largest palindrome attainable by multiplying two 3-digit integers together is:", largest_palindrome)
-
+	// solved. answer is: 906609
 }
