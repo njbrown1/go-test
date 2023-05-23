28bc15b87c3accfc24346633f3a529b8e9d1e7cd
Nathan Brown <nathanjeffreybrown@gmail.com>
2023-05-23 15:22:07 -0500

    found that the 12-digit number does not have a prime factor smaller than 37

--- proj_euler_p003.go
+++ proj_euler_p003.go
@@ -1,11 +1,28 @@
 package main
 
 import "fmt"
-import "math"
+
+// foundational thoughts:
+// we are asked to find the largest PRIME factor of 600851475143, a 12-digit number.
+// consider the case of a composite number like 58 – it is divisible by 29 and 2, which
+// are both prime. so 29 is the largest prime factor of 58. and 600851475143 is not a
+// prime number – because I plugged 600851475143 into the answer box, and it was wrong.
+
+// a composite number (ie. not a prime number) must have at least 2 composite factors.
+// simple modulo division by early prime numbers (2, 3, 5, 7...)
 
 func main() {
-	var n int = 24                            // where n is the composite number we are testing
-	var s = math.Round(math.Sqrt(float64(n))) // where s is the square root of n, rounded up
-	fmt.Println(n)
-	fmt.Println(s)
+	fmt.Println("hello world")
+	fmt.Println(600851475143 % 2)
+	fmt.Println(600851475143 % 3)
+	fmt.Println(600851475143 % 5)
+	fmt.Println(600851475143 % 7)
+	fmt.Println(600851475143 % 11)
+	fmt.Println(600851475143 % 13)
+	fmt.Println(600851475143 % 17)
+	fmt.Println(600851475143 % 19)
+	fmt.Println(600851475143 % 23)
+	fmt.Println(600851475143 % 29)
+	fmt.Println(600851475143 % 33)
+	fmt.Println(600851475143 % 37)
 }
