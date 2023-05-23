84a1074b634cefed32d2507b0f7eb460ef2839c5
Nathan Brown <nathanjeffreybrown@gmail.com>
2023-05-23 10:21:41 -0500

    added the function palindrom_check(), which doesn't really work

--- proj_euler_p004.go
+++ proj_euler_p004.go
@@ -13,12 +13,26 @@ func main() {
 	for a <= 104 {
 		for b <= 104 {
 			n = a * b
-			fmt.Println(a, ",", b, ":", n)
+			fmt.Printf("a: %d, b: %d, n: %d \n", a, b, n) // ensuring that the 'for' loop works correctly
 			b++
+
+			if n%10 != 0 {
+
+			}
 		}
 
 		b = 100
 		a++
 
 	}
+}
+
+func palindrome_check() {
+	d1 := 7
+	d2 := 6
+	d3 := 5
+	d4 := 4
+	d5 := 3
+	d6 := n % 10
+	fmt.Println(d1, d2, d3, d4, d5, d6) // doesn't work correctly, since palindrome_check() doesn't know what n is.
 }
