a241871b0b32f3f06fce060a3a2cdef6f9245b11
Nathan Brown <nathanjeffreybrown@gmail.com>
2023-05-23 09:45:24 -0500

    got the array to take the numbers, but they are in ASCII form, which isn't what I want

--- proj_euler_p004.go
+++ proj_euler_p004.go
@@ -6,22 +6,19 @@ import (
 )
 
 func main() {
-	a := 123456
+	a := 6898
 	b := [5]int{1, 2, 3, 4, 5}
 	aString := strconv.Itoa(a) // initialize aString, which is just a string (not int) equivalent of 'a'
 
 	array := make([]int, 0, len(aString))
 
-	for i := 1; i <= len(aString); i++ {
-		array = append(array, 1)
-	}
+	for i, ch := range aString {
+		// array[i] = int(ch)
+		array = append(array, int(ch))
+		fmt.Printf("Index: %d, Character: %c\n", i, ch)
+	} // this code was added by ChatGPT
 
-	// for i, ch := range aString {
-	// digit, _ := strconv.Atoi(string(ch))
-	// array[i] = digit
-	//	}
-
-	fmt.Println(array)
+	fmt.Println("initial array:", array)
 	fmt.Println("b:", b)
 	fmt.Println(aString)
 }
