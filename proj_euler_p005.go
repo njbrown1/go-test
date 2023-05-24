2431bd8f8ad8785e0b62404d268e2c909d35ea37
Nathan Brown <nathanjeffreybrown@gmail.com>
2023-05-24 10:01:05 -0500

    brute force approach failed, so going to try something different

--- proj_euler_p005.go
+++ proj_euler_p005.go
@@ -5,15 +5,15 @@ import "fmt"
 func main() {
 
 	var array []int
-	array = append(array, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
-	// array = append(array, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20)
-	number := 11
+	array = append(array, 1, 7, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20)
+	number := 4
 	counter := 0
 
-	for counter < 10 {
+	for counter < 12 { // until
+
 		counter = 0
 
-		for i := 0; i <= 9; i++ {
+		for i := 0; i <= 11; i++ {
 
 			if number%array[i] == 0 {
 				counter++
@@ -23,9 +23,11 @@ func main() {
 
 		if counter < 10 {
 			number++
+			fmt.Println(number)
 		}
 
-		fmt.Println(number)
-
 	}
+
+	fmt.Println(number)
+
 }
