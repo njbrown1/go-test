cc39757e670b95f904f0642ca44875871ffeaff9
Nathan Brown <nathanjeffreybrown@gmail.com>
2023-05-24 09:40:01 -0500

    for any given number, created a for loop that states how many of the numbers in the array (so 1-20) are evenly divisible by that number

--- proj_euler_p005.go
+++ proj_euler_p005.go
@@ -3,7 +3,18 @@ package main
 import "fmt"
 
 func main() {
+
 	var array []int
 	array = append(array, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
-	j := 10
+	array = append(array, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20)
+	number := 11
+	counter := 0
+
+	counter = 0
+	for i := 0; i <= 19; i++ {
+		if number%array[i] == 0 {
+			counter++
+		}
+	}
+	fmt.Println(counter)
 }
