865a585a25fc8461dc955166e96cc3660ebecd4c
Nathan Brown <nathanjeffreybrown@gmail.com>
2023-05-24 09:46:19 -0500

    kind of works for numbers 1-10, but it's 10 more than the stated value of 2520, so not sure where it's adding the extra 10

--- proj_euler_p005.go
+++ proj_euler_p005.go
@@ -6,15 +6,26 @@ func main() {
 
 	var array []int
 	array = append(array, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
-	array = append(array, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20)
+	// array = append(array, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20)
 	number := 11
 	counter := 0
 
-	counter = 0
-	for i := 0; i <= 19; i++ {
-		if number%array[i] == 0 {
-			counter++
+	for counter < 10 {
+		counter = 0
+		for i := 0; i <= 9; i++ {
+
+			if number%array[i] == 0 {
+				counter++
+			}
+
+			if counter < 10 {
+				number++
+			}
+
 		}
+
 	}
-	fmt.Println(counter)
+
+	fmt.Println(number)
+
 }
