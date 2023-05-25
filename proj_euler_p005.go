1fa55f8b321d13838262d984154829d2a831c5d8
Nathan Brown <nathanjeffreybrown@gmail.com>
2023-05-25 18:21:04 -0500

    created effective prime sieve

--- proj_euler_p005.go
+++ proj_euler_p005.go
@@ -2,10 +2,34 @@ package main
 
 import "fmt"
 
-func find_multiplier(i int) {
-	fmt.Println(i)
+func find_prime_factorization(i int) {
+
+	var prime_slice []int // initialize an empty slice
+	prime_slice = append(prime_slice, 2, 3, 5)
+	fmt.Println(prime_slice) // testing
+
+	for k := 4; k <= 100; k++ { // for finding prime numbers up to 100
+
+		var is_k_prime string = "yes"
+
+		for l := 0; l < len(prime_slice); l++ {
+
+			if k%prime_slice[l] == 0 {
+				is_k_prime = "no" // if k is divisible by one of the prime numbers in the slice, k is not prime.
+			}
+		}
+
+		if is_k_prime == "yes" {
+			prime_slice = append(prime_slice, k) // if k is prime, add it to prime_slice.
+		}
+
+	}
+
+	fmt.Println(prime_slice)
+
 }
 
 func main() {
-	find_multiplier(1)
+	fmt.Println("hello world")
+	find_prime_factorization(4)
 }
