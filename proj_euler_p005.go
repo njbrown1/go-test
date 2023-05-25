e55a745f16e1fcdac3a6bf88f7b83d5301e890e9
Nathan Brown <nathanjeffreybrown@gmail.com>
2023-05-25 09:59:39 -0500

    added a rudimentary for loop for finding the current_divisible_number given the first few natural numbers

--- proj_euler_p005.go
+++ proj_euler_p005.go
@@ -6,11 +6,23 @@ import "fmt"
 
 func main() {
 
+	current_divisible_number := 12
 	var slice []int
+
 	for i := 1; i <= 20; i++ { // add the numbers 1-20 to the slice
 		slice = append(slice, i)
 	}
 
 	fmt.Println(slice)
+	for j := 0; j <= 19; j++ {
+
+		current_divisor := slice[j]
+
+		if current_divisible_number%slice[j] == 0 {
+			current_divisor = (current_divisible_number / slice[j])
+			fmt.Println("current_divisor:", current_divisor, "current_divisible_number:", current_divisible_number)
+		}
+
+	}
 
 }
