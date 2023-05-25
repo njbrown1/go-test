79bb37722356be1403196fa2c230cafe89531e2b
Nathan Brown <nathanjeffreybrown@gmail.com>
2023-05-25 13:16:57 -0500

    starting from stracth with functions

--- proj_euler_p005.go
+++ proj_euler_p005.go
@@ -3,24 +3,9 @@ package main
 import "fmt"
 
 func find_multiplier(i int) {
-
-	current_multiplier := i
-
-	for a := (i - 1); a > 0; a-- {
-
-		if current_multiplier%a == 0 {
-			current_multiplier = (current_multiplier / a) // if current_multiplier is divisible by a, divide the current multiplier by a
-		}
-
-	}
-
-	fmt.Println("current_multiplier:", current_multiplier, "| for i:", i)
-
+	fmt.Println(i)
 }
 
 func main() {
-
-	for j := 2; j <= 20; j++ {
-		find_multiplier(j)
-	}
+	find_multiplier(1)
 }
