dd5e7a86a0acfded42d1d489ef3992de45a47535
Nathan Brown <nathanjeffreybrown@gmail.com>
2023-05-25 11:41:08 -0500

    started function find_multiplier

--- proj_euler_p005.go
+++ proj_euler_p005.go
@@ -4,6 +4,13 @@ import "fmt"
 
 // import "math"
 
+func find_multiplier(i int) {
+	for a := i; a > 0; a-- {
+
+	}
+
+}
+
 func main() {
 
 	var slice []int
@@ -15,16 +22,5 @@ func main() {
 	for i := 4; i <= 20; i++ { // for the numbers 4-x (here, 20)
 
 		slice = append(slice, i) // add i to the slice
-		current_multiplier := i
-
-		fmt.Println(i)
-
-		for a := 0; a < len(slice); a++ {
-
-			if current_multiplier%slice[a] == 0 {
-				current_multiplier = (current_multiplier / slice[a])
-				fmt.Println("i:", i, "current_multiplier:", current_multiplier)
-			}
-		}
 	}
 }
