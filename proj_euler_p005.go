2438c80b2b6d5507d062ebf9da51a07c30752a24
Nathan Brown <nathanjeffreybrown@gmail.com>
2023-05-25 09:32:36 -0500

    added code comments and cleaned up brackets

--- proj_euler_p005.go
+++ proj_euler_p005.go
@@ -4,31 +4,11 @@ import "fmt"
 
 func main() {
 
-	var array []int
-	array = append(array, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
-	array = append(array, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20)
-	number := 2520
-	counter := 0
-
-	for counter < 20 { // until
-
-		counter = 0
-
-		for i := 0; i <= 19; i++ {
-
-			if number%array[i] == 0 {
-				counter++
-			}
-
-		}
-
-		if counter < 20 {
-			number = number + (2520 * 11 * 13 * 17 * 19)
-			fmt.Println(number)
-		}
-
+	var slice []int
+	for i := 1; i <= 20; i++ { // add the numbers 1-20 to the slice
+		slice = append(slice, i)
 	}
 
-	fmt.Println(number)
+	fmt.Println(slice)
 
 }
