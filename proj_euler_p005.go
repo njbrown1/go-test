05ad9a507fcaafd751c193ff13051a6ee5e5051a
Nathan Brown <nathanjeffreybrown@gmail.com>
2023-05-25 19:36:55 -0500

    modified generate_primes function + added find_prime_factorization function

--- proj_euler_p005.go
+++ proj_euler_p005.go
@@ -2,42 +2,40 @@ package main
 
 import "fmt"
 
-func find_prime_factorization(i int) {
+func generate_primes(limit int) []int {
 
-	// BEGINNING of prime sieve
+	// this is a classic prime sieve.
 
-	var prime_slice []int                      // initialize an empty slice
-	prime_slice = append(prime_slice, 2, 3, 5) // add the first few primes
+	var prime_slice []int
+	prime_slice = append(prime_slice, 2, 3, 5) // add the first few primes.
 
-	for k := 4; k <= 100; k++ { // for finding prime numbers up to 100
+	for k := 4; k <= limit; k++ { // for finding prime numbers up to 'limit'.
 
-		var is_k_prime string = "yes"
+		is_k_prime := true
 
 		for l := 0; l < len(prime_slice); l++ { // iterate over all of the items in prime_slice.
 
 			if k%prime_slice[l] == 0 {
-				is_k_prime = "no" // if k is divisible by one of the prime numbers in the slice, k is NOT prime.
+				is_k_prime = false // if k is divisible by one of the prime numbers in the slice, k is NOT prime.
 			}
 		}
 
-		if is_k_prime == "yes" {
+		if is_k_prime == true {
 			prime_slice = append(prime_slice, k) // if k IS prime, add it to prime_slice.
+
 		}
 	}
 
-	fmt.Println(prime_slice)
+	return prime_slice
 
-	// END of prime sieve
+}
 
-	storage := make(map[int]int)
-	storage[1] = 2
-	storage[2] = 3
-
-	fmt.Println(storage[2])
-
+func find_prime_factorization() {
+	fmt.Println(true)
 }
 
 func main() {
 	fmt.Println("hello world")
-	find_prime_factorization(4)
+	result := generate_primes(1000)
+	fmt.Println(result)
 }
