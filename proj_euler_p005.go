0bc726d1a9b06d5e768ae3debc32510006c5cf7e
Nathan Brown <nathanjeffreybrown@gmail.com>
2023-05-25 19:21:14 -0500

    now working with maps

--- proj_euler_p005.go
+++ proj_euler_p005.go
@@ -4,28 +4,36 @@ import "fmt"
 
 func find_prime_factorization(i int) {
 
-	var prime_slice []int // initialize an empty slice
-	prime_slice = append(prime_slice, 2, 3, 5)
-	fmt.Println(prime_slice) // testing
+	// BEGINNING of prime sieve
+
+	var prime_slice []int                      // initialize an empty slice
+	prime_slice = append(prime_slice, 2, 3, 5) // add the first few primes
 
 	for k := 4; k <= 100; k++ { // for finding prime numbers up to 100
 
 		var is_k_prime string = "yes"
 
-		for l := 0; l < len(prime_slice); l++ {
+		for l := 0; l < len(prime_slice); l++ { // iterate over all of the items in prime_slice.
 
 			if k%prime_slice[l] == 0 {
-				is_k_prime = "no" // if k is divisible by one of the prime numbers in the slice, k is not prime.
+				is_k_prime = "no" // if k is divisible by one of the prime numbers in the slice, k is NOT prime.
 			}
 		}
 
 		if is_k_prime == "yes" {
-			prime_slice = append(prime_slice, k) // if k is prime, add it to prime_slice.
+			prime_slice = append(prime_slice, k) // if k IS prime, add it to prime_slice.
 		}
-
 	}
 
 	fmt.Println(prime_slice)
+
+	// END of prime sieve
+
+	storage := make(map[int]int)
+	storage[1] = 2
+	storage[2] = 3
+
+	fmt.Println(storage[2])
 
 }
 
