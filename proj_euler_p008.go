56f71eab03d0704fc97aaa80fe45fcf86f898303
Nathan Brown <nathanjeffreybrown@gmail.com>
2023-05-27 12:16:32 -0500

    starting to find products with for loops

--- proj_euler_p008.go
+++ proj_euler_p008.go
@@ -3,15 +3,21 @@ package main
 import (
 	"fmt"
 	"os"
+	"strings"
 )
 
 func main() {
-	content, err := os.ReadFile("1000_digit_number.txt")
-	if err != nil {
-		fmt.Println("Error reading file:", err)
-		return
-	}
 
-	number := string(content)
-	fmt.Println("Number:", number)
+	data, _ := os.ReadFile("1000_digit_number.txt")                               // read the file with the 1000 digit number
+	var thousand_digit_number string = strings.ReplaceAll(string(data), "\n", "") // remove newline characters from the data
+
+	fmt.Println(thousand_digit_number)
+
+	for i := 0; i <= 4; i++ { // start with the index i
+
+		for j := 0; j <= 3; j++ {
+			character := thousand_digit_number[i+j]
+			fmt.Println(i, j, string(character))
+		}
+	}
 }
