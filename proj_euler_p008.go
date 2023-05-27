e1b10dea53763043d28e4221163d8187d775b891
Nathan Brown <nathanjeffreybrown@gmail.com>
2023-05-27 13:05:21 -0500

    largest_product function now works successfully

--- proj_euler_p008.go
+++ proj_euler_p008.go
@@ -7,28 +7,33 @@ import (
 	"strings"
 )
 
-func find_product(thousand_digit_number string, number_of_consecutive_digits int) int {
+func find_product(thousand_digit_number string, number_of_consecutive_digits int) int { // outputs: the largest product
 
-	var slice_of_x_numbers = make([]int, number_of_consecutive_digits)
+	var slice_of_x_numbers []int // initialize an empty slice
 
-	current_product := 1
+	largest_product := 0
 
-	for i := 0; i <= 12; i++ {
+	for i := 0; i <= 12; i++ { // 12 is arbitrary
 
-		current_product = 1
+		current_product := 1
 
-		for j := 0; j <= (number_of_consecutive_digits - 1); j++ {
+		for j := 0; j <= (number_of_consecutive_digits - 1); j++ { // add numbers to the slice
 
 			character, _ := strconv.Atoi(string(thousand_digit_number[i+j]))
-			slice_of_x_numbers[j] = character
+			slice_of_x_numbers = append(slice_of_x_numbers, character) // add one number to the slice
+			current_product = current_product * character
 			fmt.Println(i, j, slice_of_x_numbers, current_product)
 
 		}
 
+		if current_product > largest_product { // update largest product if there is a new highest product
+			largest_product = current_product
+		}
+
+		slice_of_x_numbers = nil // clear the slice
+
 	}
-
-	return current_product
-
+	return largest_product
 }
 
 func main() {
