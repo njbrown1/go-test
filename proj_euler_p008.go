e55cf87c26650b3465c7aa43ac0aa4279c33a84f
Nathan Brown <nathanjeffreybrown@gmail.com>
2023-05-27 13:11:12 -0500

    added code comments and cleaned up code. problem solved.

--- proj_euler_p008.go
+++ proj_euler_p008.go
@@ -7,7 +7,7 @@ import (
 	"strings"
 )
 
-func find_product(thousand_digit_number string, number_of_consecutive_digits int) int { // outputs: the largest product
+func find_product(thousand_digit_number string, number_of_consecutive_digits int) int { // outputs the largest product
 
 	var slice_of_x_numbers []int // initialize an empty slice
 
@@ -15,14 +15,14 @@ func find_product(thousand_digit_number string, number_of_consecutive_digits int
 
 	for i := 0; i <= (1000 - number_of_consecutive_digits); i++ { // to avoid an index-out-of-range error
 
-		current_product := 1
+		current_product := 1 // 1 to avoid problems with multiplication
 
 		for j := 0; j <= (number_of_consecutive_digits - 1); j++ { // add numbers to the slice
 
-			character, _ := strconv.Atoi(string(thousand_digit_number[i+j]))
-			slice_of_x_numbers = append(slice_of_x_numbers, character) // add one number to the slice
-			current_product = current_product * character
-			fmt.Println(i, j, slice_of_x_numbers, current_product) // for debugging purposes
+			character, _ := strconv.Atoi(string(thousand_digit_number[i+j])) // get the character (and discard the error)
+			slice_of_x_numbers = append(slice_of_x_numbers, character)       // add one number to the slice
+			current_product = current_product * character                    // update current_product
+			fmt.Println(i, j, slice_of_x_numbers, current_product)           // for debugging purposes
 
 		}
 
@@ -33,7 +33,9 @@ func find_product(thousand_digit_number string, number_of_consecutive_digits int
 		slice_of_x_numbers = nil // clear the slice
 
 	}
+
 	return largest_product
+
 }
 
 func main() {
@@ -41,6 +43,6 @@ func main() {
 	data, _ := os.ReadFile("1000_digit_number.txt")                               // read the file with the 1000 digit number
 	var thousand_digit_number string = strings.ReplaceAll(string(data), "\n", "") // remove newline characters from the data
 	fmt.Println(thousand_digit_number)                                            // debugging purposes
-	fmt.Println(find_product(thousand_digit_number, 13))
+	fmt.Println(find_product(thousand_digit_number, 13))                          // solved! answer: 23514624000
 
 }
