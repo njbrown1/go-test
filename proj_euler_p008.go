e3f9414fcbc27d42cfbd5569d788cc2833cfeeb1
Nathan Brown <nathanjeffreybrown@gmail.com>
2023-05-27 12:28:04 -0500

    added slice and starting to add numbers to them

--- proj_euler_p008.go
+++ proj_euler_p008.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
+	"strconv"
 	"strings"
 )
 
@@ -11,13 +12,17 @@ func main() {
 	data, _ := os.ReadFile("1000_digit_number.txt")                               // read the file with the 1000 digit number
 	var thousand_digit_number string = strings.ReplaceAll(string(data), "\n", "") // remove newline characters from the data
 
+	x := 4
+	var slice_of_x_numbers = make([]int, x)
+	// largest_product := 0
 	fmt.Println(thousand_digit_number)
 
-	for i := 0; i <= 4; i++ { // start with the index i
+	for i := 0; i <= 4; i++ { // this
 
 		for j := 0; j <= 3; j++ {
-			character := thousand_digit_number[i+j]
-			fmt.Println(i, j, string(character))
+			character, _ := strconv.Atoi(string(thousand_digit_number[i+j]))
+			slice_of_x_numbers[j] = character
+			fmt.Println(i, j, slice_of_x_numbers)
 		}
 	}
 }
