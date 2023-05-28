7998a561ff33cba50bf461df49bdf646d6a85905
Nathan Brown <nathanjeffreybrown@gmail.com>
2023-05-28 17:08:42 -0500

    started to add numbers to a slice in problem 11

--- proj_euler_p011.go
+++ proj_euler_p011.go
@@ -1,7 +1,41 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
 
 func main() {
-	fmt.Println("hello world")
+
+	grid_file, _ := os.ReadFile("20x20_grid.txt")                      // grab the numbers from the file
+	var data string = strings.ReplaceAll(string(grid_file), "\n", " ") // convert newline characters to spaces
+	var initial_slice []int
+
+	fmt.Println(string(data)) // for debugging
+	fmt.Println(len(data))    // for debugging
+
+	for i := 0; i <= 57; {
+
+		first_digit, _ := strconv.Atoi(string(data[i]))
+		second_digit, _ := strconv.Atoi(string(data[i+1]))
+		initial_slice = append(initial_slice, first_digit*10+second_digit)
+		i = i + 3
+		fmt.Printf("%02d\n", initial_slice) // for debugging
+	}
+
+	var twoD [2][3]int
+	for i := 0; i < 2; i++ {
+		for j := 0; j < 3; j++ {
+			twoD[i][j] = i + j
+		}
+	}
+	fmt.Println("2d: ", twoD)
 }
+
+// I need four functions:
+// 1. a function that finds the largest product for numbers up / down
+// 2. a function that finds the largest product for numbers left / right
+// 3. a function that finds the largest product for numbers diagonally, up and to the right
+// 4. a function that finds the largest product for numbers diagonally, up and to the left
