40f5c1adf92cc244918faa0c724d21f1c2c6408b
Nathan Brown <nathanjeffreybrown@gmail.com>
2023-05-29 18:00:52 -0500

    function now works correctly

--- proj_euler_p011.go
+++ proj_euler_p011.go
@@ -30,27 +30,40 @@ func main() {
 
 func find_largest_product_up_and_down(grid [][]int) int { // input: the two_dimensional_slice (grid) of numbers. output: the largest product.
 
+	current_largest_product := 0
+
 	for row := 0; row <= 19; row++ {
 		for column := 0; column <= 19; column++ {
 
 			x := column
 			y := row
-			out_of_range := (x < 0 || x > 19) || (y < 0 || y > 19)
+			out_of_range := (x < 0 || x > 19) || (y < 0 || y > 19) // a boolean that returns true if the requested value is out of range
 			var four_number_slice []int
 
 			for k := 1; k <= 4; k++ {
 				if out_of_range == true {
 					break
 				}
-				four_number_slice = append(four_number_slice, grid[y][x])
-				x++
+				four_number_slice = append(four_number_slice, grid[x][y])
+				x++ // changes based on the function I'm using
 				out_of_range = (x < 0 || x > 19) || (y < 0 || y > 19)
 			}
 
-			fmt.Println(four_number_slice)
+			// the code below multiplies together the numbers in the four_number_slice, then updates the current_largest_product if product > current_largest_product.
+
+			product := 1
+			for j := 0; j <= len(four_number_slice)-1; j++ {
+				product = product * four_number_slice[j]
+			}
+
+			if product > current_largest_product {
+				current_largest_product = product
+			}
+
+			fmt.Println(four_number_slice, product) // for debugging
 		}
 	}
-	return 1024
+	return current_largest_product
 }
 
 // I need four functions:
