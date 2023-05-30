7b26fab33177afdb5fa0a961cdfca15c33a17ace
Nathan Brown <nathanjeffreybrown@gmail.com>
2023-05-29 19:09:54 -0500

    I thought this would work, but it doesn't. diagnosing the problem.

--- proj_euler_p011.go
+++ proj_euler_p011.go
@@ -25,18 +25,25 @@ func main() {
 			two_dimensional_slice[i][j] = (tens_digit * 10) + (ones_digit)        // assign the two-digit number to the two-dimensional array
 		}
 	}
-	fmt.Println(find_largest_product_up_and_down(two_dimensional_slice)) // necessary
+	fmt.Println(find_largest_product_up_and_down(two_dimensional_slice))
+	fmt.Println(find_largest_product_left_and_right(two_dimensional_slice))
+	fmt.Println(find_largest_product_up_right_diagonally(two_dimensional_slice))
+	fmt.Println(find_largest_product_down_right_diagonally(two_dimensional_slice))
 }
+
+// NOTES ON THE FOUR FUNCTIONS:
+// 1. I first wrote the function find_largest_product_up_and_down, and I wanted to make the function extremely reusable.
+// The only difference between all four functions is the presence of y++, y--, x++, and x--.
 
 func find_largest_product_up_and_down(grid [][]int) int { // input: the two_dimensional_slice (grid) of numbers. output: the largest product.
 
 	current_largest_product := 0
 
 	for row := 0; row <= 19; row++ {
-		for column := 0; column <= 19; column++ {
+		for column := 0; column <= 19; column++ { // access EVERY number in the grid
 
-			x := column
-			y := row
+			x := column                                            // so that x can be adjusted without adjusting 'column'
+			y := row                                               // so that y can be adjusted without adjusting 'row'
 			out_of_range := (x < 0 || x > 19) || (y < 0 || y > 19) // a boolean that returns true if the requested value is out of range
 			var four_number_slice []int
 
@@ -44,8 +51,8 @@ func find_largest_product_up_and_down(grid [][]int) int { // input: the two_dime
 				if out_of_range == true {
 					break
 				}
-				four_number_slice = append(four_number_slice, grid[x][y])
-				x++ // changes based on the function I'm using
+				four_number_slice = append(four_number_slice, grid[y][x])
+				y++
 				out_of_range = (x < 0 || x > 19) || (y < 0 || y > 19)
 			}
 
@@ -58,16 +65,122 @@ func find_largest_product_up_and_down(grid [][]int) int { // input: the two_dime
 
 			if product > current_largest_product {
 				current_largest_product = product
+				fmt.Println(four_number_slice, product)
 			}
-
-			fmt.Println(four_number_slice, product) // for debugging
 		}
 	}
 	return current_largest_product
 }
 
-// I need four functions:
-// 1. a function that finds the largest product for numbers up / down
-// 2. a function that finds the largest product for numbers left / right
-// 3. a function that finds the largest product for numbers diagonally, up and to the right
-// 4. a function that finds the largest product for numbers diagonally, up and to the left
+func find_largest_product_left_and_right(grid [][]int) int { // input: the two_dimensional_slice (grid) of numbers. output: the largest product.
+
+	current_largest_product := 0
+
+	for row := 0; row <= 19; row++ {
+		for column := 0; column <= 19; column++ { // access EVERY number in the grid
+
+			x := column                                            // so that x can be adjusted without adjusting 'column'
+			y := row                                               // so that y can be adjusted without adjusting 'row'
+			out_of_range := (x < 0 || x > 19) || (y < 0 || y > 19) // a boolean that returns true if the requested value is out of range
+			var four_number_slice []int
+
+			for k := 1; k <= 4; k++ {
+				if out_of_range == true {
+					break
+				}
+				four_number_slice = append(four_number_slice, grid[y][x])
+				x++
+				out_of_range = (x < 0 || x > 19) || (y < 0 || y > 19)
+			}
+
+			// the code below multiplies together the numbers in the four_number_slice, then updates the current_largest_product if product > current_largest_product.
+
+			product := 1
+			for j := 0; j <= len(four_number_slice)-1; j++ {
+				product = product * four_number_slice[j]
+			}
+
+			if product > current_largest_product {
+				current_largest_product = product
+				fmt.Println(four_number_slice, product)
+			}
+		}
+	}
+	return current_largest_product
+}
+
+func find_largest_product_up_right_diagonally(grid [][]int) int { // input: the two_dimensional_slice (grid) of numbers. output: the largest product.
+
+	current_largest_product := 0
+
+	for row := 0; row <= 19; row++ {
+		for column := 0; column <= 19; column++ { // access EVERY number in the grid
+
+			x := column                                            // so that x can be adjusted without adjusting 'column'
+			y := row                                               // so that y can be adjusted without adjusting 'row'
+			out_of_range := (x < 0 || x > 19) || (y < 0 || y > 19) // a boolean that returns true if the requested value is out of range
+			var four_number_slice []int
+
+			for k := 1; k <= 4; k++ {
+				if out_of_range == true {
+					break
+				}
+				four_number_slice = append(four_number_slice, grid[y][x])
+				x++
+				y--
+				out_of_range = (x < 0 || x > 19) || (y < 0 || y > 19)
+			}
+
+			// the code below multiplies together the numbers in the four_number_slice, then updates the current_largest_product if product > current_largest_product.
+
+			product := 1
+			for j := 0; j <= len(four_number_slice)-1; j++ {
+				product = product * four_number_slice[j]
+			}
+
+			if product > current_largest_product {
+				current_largest_product = product
+				fmt.Println(four_number_slice, product)
+			}
+		}
+	}
+	return current_largest_product
+}
+
+func find_largest_product_down_right_diagonally(grid [][]int) int { // input: the two_dimensional_slice (grid) of numbers. output: the largest product.
+
+	current_largest_product := 0
+
+	for row := 0; row <= 19; row++ {
+		for column := 0; column <= 19; column++ { // access EVERY number in the grid
+
+			x := column                                            // so that x can be adjusted without adjusting 'column'
+			y := row                                               // so that y can be adjusted without adjusting 'row'
+			out_of_range := (x < 0 || x > 19) || (y < 0 || y > 19) // a boolean that returns true if the requested value is out of range
+			var four_number_slice []int
+
+			for k := 1; k <= 4; k++ {
+				if out_of_range == true {
+					break
+				}
+				four_number_slice = append(four_number_slice, grid[y][x])
+				x++
+				y++
+				out_of_range = (x < 0 || x > 19) || (y < 0 || y > 19)
+			}
+
+			// the code below multiplies together the numbers in the four_number_slice, then updates the current_largest_product if product > current_largest_product.
+
+			product := 1
+			for j := 0; j <= len(four_number_slice)-1; j++ {
+				product = product * four_number_slice[j]
+			}
+
+			if product > current_largest_product {
+				current_largest_product = product
+				fmt.Println(four_number_slice, product)
+			}
+		}
+	}
+	return current_largest_product
+}
