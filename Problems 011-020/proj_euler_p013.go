c7326671641a47adcc36473ec76da6646a209bf5
Nathan Brown <nathanjeffreybrown@gmail.com>
2023-06-04 14:08:06 -0500

    changed location of 100_fifty_digit_numbers file to see if that solves my problem

--- Problems 011-020/proj_euler_p013.go
+++ Problems 011-020/proj_euler_p013.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"strings"
+)
 
 func main() {
 	fmt.Println("hello world")
+
+	numbers_file, _ := os.ReadFile("100_fifty_digit_numbers.txt")                         // grab the numbers from the file
+	var data_without_newlines string = strings.ReplaceAll(string(numbers_file), "\n", "") // remove newline characters
+	var condensed_numbers string = strings.ReplaceAll(string(data_without_newlines), " ", "")
+
+	fmt.Println(condensed_numbers)
+	fmt.Println(numbers_file)
 }
