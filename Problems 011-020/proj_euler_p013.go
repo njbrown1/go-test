641f6adbfcee69ce58fcd97cfbe126d104f73aa8
Nathan Brown <nathanjeffreybrown@gmail.com>
2023-06-04 15:13:33 -0500

    added code comments for p#013

--- Problems 011-020/proj_euler_p013.go
+++ Problems 011-020/proj_euler_p013.go
@@ -16,7 +16,7 @@ func main() {
 	var fifty_digit_number_slice []int
 	sum_of_numbers := 0
 
-	for i := 0; i <= 4999; i = i + 50 {
+	for i := 0; i <= 4999; i = i + 50 { // this function just adds the first 13 digits of each of the 50 digit numbers to fifty_digit_number_slice
 		var fifty_digit_string string
 		for j := 0; j <= 12; j++ {
 			fifty_digit_string += string(condensed_numbers[i+j])
@@ -27,8 +27,9 @@ func main() {
 	}
 
 	for i := 0; i <= 99; i++ {
-		sum_of_numbers = sum_of_numbers + fifty_digit_number_slice[i]
+		sum_of_numbers = sum_of_numbers + fifty_digit_number_slice[i] // add up all of the numbers in the fifty_digit_number_slice
 	}
 
 	fmt.Println("the sum of the numbers is:", sum_of_numbers)
+	// solved! answer: 5537376230
 }
