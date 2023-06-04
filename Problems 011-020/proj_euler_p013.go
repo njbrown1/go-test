60443026d7db85df8ef75bc0286c76cdfead80c7
Nathan Brown <nathanjeffreybrown@gmail.com>
2023-06-04 14:22:10 -0500

    semi-functional. imported the file correctly, and figuring out how to add each number to an array

--- Problems 011-020/proj_euler_p013.go
+++ Problems 011-020/proj_euler_p013.go
@@ -3,16 +3,27 @@ package main
 import (
 	"fmt"
 	"os"
+	"strconv"
 	"strings"
 )
 
 func main() {
+
+	numbers_file, _ := os.ReadFile("100_fifty_digit_numbers.txt")                             // grab the numbers from the file
+	var data_without_newlines string = strings.ReplaceAll(string(numbers_file), "\n", "")     // remove newline characters
+	var condensed_numbers string = strings.ReplaceAll(string(data_without_newlines), " ", "") // remove spaces
+
 	fmt.Println("hello world")
-
-	numbers_file, _ := os.ReadFile("100_fifty_digit_numbers.txt")                         // grab the numbers from the file
-	var data_without_newlines string = strings.ReplaceAll(string(numbers_file), "\n", "") // remove newline characters
-	var condensed_numbers string = strings.ReplaceAll(string(data_without_newlines), " ", "")
-
 	fmt.Println(condensed_numbers)
-	fmt.Println(numbers_file)
+
+	var fifty_digit_number_slice []int
+
+	for i := 0; i <= 49; i++ {
+		one_number_only, _ := strconv.Atoi(string(condensed_numbers[i]))
+		fifty_digit_number_slice = append(fifty_digit_number_slice, one_number_only)
+
+		// strconv.Atoi(string(condensed_data[(40*i)+(2*j)]))
+	}
+
+	fmt.Println(fifty_digit_number_slice)
 }
