36fc4bb24fb7a4a8a4d8bd2fde3b7a62a3b6e4f2
Nathan Brown <nathanjeffreybrown@gmail.com>
2023-06-04 14:44:57 -0500

    forgot to account for the maximum int value, which is 9223372036854775807. need to fix

--- Problems 011-020/proj_euler_p013.go
+++ Problems 011-020/proj_euler_p013.go
@@ -13,17 +13,15 @@ func main() {
 	var data_without_newlines string = strings.ReplaceAll(string(numbers_file), "\n", "")     // remove newline characters
 	var condensed_numbers string = strings.ReplaceAll(string(data_without_newlines), " ", "") // remove spaces
 
-	fmt.Println("hello world")
-	fmt.Println(condensed_numbers)
+	// var fifty_digit_number_slice []int
 
-	var fifty_digit_number_slice []int
-
-	for i := 0; i <= 49; i++ {
-		one_number_only, _ := strconv.Atoi(string(condensed_numbers[i]))
-		fifty_digit_number_slice = append(fifty_digit_number_slice, one_number_only)
-
-		// strconv.Atoi(string(condensed_data[(40*i)+(2*j)]))
+	for i := 0; i <= 4999; i = i + 50 {
+		var fifty_digit_string string
+		for j := 0; j <= 49; j++ {
+			fifty_digit_string += string(condensed_numbers[i+j])
+		}
+		fmt.Println(fifty_digit_string)
+		number, _ := strconv.Atoi(fifty_digit_string)
+		fmt.Println(int(number))
 	}
-
-	fmt.Println(fifty_digit_number_slice)
 }
