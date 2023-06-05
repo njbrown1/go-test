7644e625cd33594a836b21ae1a2a7d59a17e5cb5
Nathan Brown <nathanjeffreybrown@gmail.com>
2023-06-04 19:15:14 -0500

    solved p#014

--- Problems 011-020/proj_euler_p014.go
+++ Problems 011-020/proj_euler_p014.go
@@ -2,11 +2,10 @@ package main
 
 import "fmt"
 
-func find_length_of_collatz_sequence(number int) int {
+func find_length_of_collatz_sequence(number int) int { // this function takes an integer as input, and outputs the number of terms for that integer's Collatz sequence.
 	number_of_terms := 1
 	for number_equals_1 := false; number_equals_1 != true; number_of_terms++ {
-		fmt.Println(number) // for debugging
-		if number == 1 {    // if the number equals 1, break the for loop
+		if number == 1 { // if the number equals 1, break the 'for' loop
 			break
 		} else if number%2 == 0 { // if n is even: n -> n/2
 			number = (number / 2)
@@ -18,7 +17,12 @@ func find_length_of_collatz_sequence(number int) int {
 }
 
 func main() {
-	fmt.Println("hello world")
-	result := find_length_of_collatz_sequence(5)
-	fmt.Println("number of terms:", result)
+	longest_number_of_terms := 0
+	for i := 1; i <= 999999; i++ {
+		number_of_terms_result := find_length_of_collatz_sequence(i)
+		if number_of_terms_result > longest_number_of_terms {
+			longest_number_of_terms = number_of_terms_result
+			fmt.Println(i, "has", number_of_terms_result, "terms in its Collatz sequence")
+		}
+	}
 }
