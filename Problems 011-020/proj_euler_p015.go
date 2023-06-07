121c815e64dd87c2767c2dcd93b8b3042d3bb623
Nathan Brown <nathanjeffreybrown@gmail.com>
2023-06-07 11:11:08 -0500

    changed output of the factorial function to a BigInt, instead of a string

--- Problems 011-020/proj_euler_p015.go
+++ Problems 011-020/proj_euler_p015.go
@@ -5,17 +5,20 @@ import (
 	"math/big"
 )
 
-func factorialOfNumber(number int64) string {
+func factorial_of_number(number int64) *big.Int {
+
+	// BigInts are required – instead of int64 – because the factorial of
+	// numbers > 30 exceeds the integer limit for int64.
 
 	product := big.NewInt(1)
 	for i := 1; i <= int(number); i++ {
 		product.Mul(product, big.NewInt(int64(i)))
 	}
-	return_string := product.String()
-	return return_string
+
+	return product
 }
 
 func main() {
-	result := factorialOfNumber(40)
+	result := factorial_of_number(40)
 	fmt.Println(result)
 }
