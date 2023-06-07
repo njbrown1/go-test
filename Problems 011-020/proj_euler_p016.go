aff6f3b25ff04f5a1880feae1277fe8ba5ffcecf
Nathan Brown <nathanjeffreybrown@gmail.com>
2023-06-07 12:02:54 -0500

    solved p#016

--- Problems 011-020/proj_euler_p016.go
+++ Problems 011-020/proj_euler_p016.go
@@ -1,9 +1,23 @@
 package main
 
+import "math/big"
 import "fmt"
+import "strconv"
 
 func main() {
-	for i := 1; i <= 40; i++ {
-		fmt.Println()
+	base := big.NewInt(2)
+	exponent := big.NewInt(1000)
+	result := big.NewInt(0)
+
+	result.Exp(base, exponent, nil)
+
+	string_of_result := result.String()
+
+	var current_sum int64 = 0
+	for i := 0; i < len(string_of_result); i++ {
+		digit, _ := strconv.Atoi(string(string_of_result[i]))
+		current_sum += int64(digit)
 	}
+
+	fmt.Println(current_sum)
 }
