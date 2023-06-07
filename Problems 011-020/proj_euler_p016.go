328fc7c9cf686fdb2fe150b7585ba15d2476a9f2
Nathan Brown <nathanjeffreybrown@gmail.com>
2023-06-07 12:09:05 -0500

    added more code comments and cleaned up code – problem successfully solved

--- Problems 011-020/proj_euler_p016.go
+++ Problems 011-020/proj_euler_p016.go
@@ -1,23 +1,24 @@
 package main
 
-import "math/big"
-import "fmt"
-import "strconv"
+import (
+	"fmt"
+	"math/big"
+	"strconv"
+)
 
 func main() {
-	// set basic variables, all in BigInt to eliminate conversion issues
+	// set basic variables, all in BigInt to eliminate conversion issues.
 	base := big.NewInt(2)
 	exponent := big.NewInt(1000)
 	result := big.NewInt(0)
 
-	result.Exp(base, exponent, nil)
-	string_of_result := result.String()
+	result.Exp(base, exponent, nil)     // finds the result. very fast too: took less than 2 seconds.
+	string_of_result := result.String() // turns the result into a string.
 
 	var current_sum int64 = 0
-	for i := 0; i < len(string_of_result); i++ { // add each digit of the string to the current_sum
+	for i := 0; i < len(string_of_result); i++ { // add each digit of the string to current_sum.
 		digit, _ := strconv.Atoi(string(string_of_result[i]))
 		current_sum += int64(digit)
 	}
-
 	fmt.Println(current_sum) // solved! answer: 1366
 }
