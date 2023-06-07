c796e9f0831f81fbd86d8e841720d32dd60443fd
Nathan Brown <nathanjeffreybrown@gmail.com>
2023-06-07 18:03:59 -0500

    worked on creating a map of spellings

--- Problems 011-020/proj_euler_p017.go
+++ Problems 011-020/proj_euler_p017.go
@@ -21,6 +21,22 @@ func add_leading_zeros(number int) string { // takes any number from 0-999, and 
 
 func find_length_of_written_number(three_digit_number string) int {
 	// length_of_written_number := 0
+
+	spellings := make(map[int]int)
+	one_digit_words := []string{"one", "two", "three", "four", "five", "six", "seven", "eight", "nine"}
+	tens_words := []string{"twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"}
+	irregular_words := []string{"eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"}
+
+	for i, word := range one_digit_words {
+		spellings[i+1] = len(word)
+	}
+	for i, word := range tens_words {
+		spellings[(i+2)*10] = len(word)
+	}
+	for i, word := range irregular_words {
+		spellings[(i + 11)] = len(word)
+	}
+
 	hundreds_digit, _ := strconv.Atoi(string(three_digit_number[0]))
 	tens_digit, _ := strconv.Atoi(string(three_digit_number[1]))
 	ones_digit, _ := strconv.Atoi(string(three_digit_number[2]))
