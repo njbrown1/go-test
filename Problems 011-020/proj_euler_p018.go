55b97d5bb9023f266e29bc9d6721567713f335ad
Nathan Brown <nathanjeffreybrown@gmail.com>
2023-06-10 13:09:32 -0500

    don't know how to add the numbers to the two-dimensional slice yet, but I do have the correct dimensions for the two-dimensional slice, which is cool

--- Problems 011-020/proj_euler_p018.go
+++ Problems 011-020/proj_euler_p018.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
 
 func main() {
-	fmt.Println("hello world")
+	numbers_file, _ := os.ReadFile("triangle_numbers.txt")                                    // grab the numbers from the file
+	var data_without_newlines string = strings.ReplaceAll(string(numbers_file), "\n", "")     // remove newline characters
+	var condensed_numbers string = strings.ReplaceAll(string(data_without_newlines), " ", "") // remove spaces
+	fmt.Println(condensed_numbers)
+
+	two_dimensional_slice := make([][]int, 15)
+
+	for i := 0; i <= 14; i++ {
+		two_dimensional_slice[i] = make([]int, i+1)
+		for j := 0; j <= i; j++ {
+			tens_digit, _ := strconv.Atoi(string(condensed_numbers[2]))
+			ones_digit, _ := strconv.Atoi(string(condensed_numbers[2]))
+			two_dimensional_slice[i][j] = (tens_digit * 10) + (ones_digit) // assign the two-digit number to the two-dimensional array
+		}
+	}
+
+	fmt.Println(two_dimensional_slice)
+	fmt.Println(len(two_dimensional_slice[14]))
 }
