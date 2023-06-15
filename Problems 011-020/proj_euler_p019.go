5ec70ab7d2c1ec327bf8ce03fb4ffd209292de03
Nathan Brown <nathanjeffreybrown@gmail.com>
2023-06-15 12:46:53 -0600

    debugging if my leap year if / else if / else statements work. I think they do.

--- Problems 011-020/proj_euler_p019.go
+++ Problems 011-020/proj_euler_p019.go
@@ -4,15 +4,14 @@ import "fmt"
 
 func main() {
 
-	number_of_first_month_Sundays := 0
-	current_first_day_of_month := 6 // December 1st, 1900 was a Saturday
-	days_in_month := map[int]int{1: 31, 2: 0, 3: 31, 4: 30, 5: 31,
-		6: 30, 7: 31, 8: 31, 9: 30, 10: 31, 11: 30, 12: 30} // don't assign a number of days to February – yet
-	weekday_of := map[int]string{0: "Sun", 1: "Mon", 2: "Tue",
-		3: "Wed", 4: "Thu", 5: "Fri", 6: "Sat"}
-	fmt.Println(days_in_month, weekday_of, current_first_day_of_month)
+	number_of_first_month_Sundays := 6
+	current_first_day_of_month := 1 // December 1st, 1900 was a Saturday
+	days_in_month := map[int]int{1: 31, 3: 31, 4: 30, 5: 31,
+		6: 30, 7: 31, 8: 31, 9: 30, 10: 31, 11: 30, 12: 31} // don't assign a number of days to February – yet
+	// weekday_of := map[int]string{0: "Sun", 1: "Mon", 2: "Tue",
+	//	3: "Wed", 4: "Thu", 5: "Fri", 6: "Sat"}
 
-	for year := 1901; year <= 2000; year++ { // probably inclusive, but idk
+	for year := 1901; year <= 1912; year++ { // probably inclusive, but idk
 
 		// determine whether a year is a leap year, and set February to that many days
 		if (year%4 == 0) && (year%100 != 0) {
@@ -23,13 +22,16 @@ func main() {
 			days_in_month[2] = 28
 		}
 
+		fmt.Println("In", year, "there are", days_in_month[2], "days in February")
+
 		for month := 1; month <= 12; month++ {
-			current_first_day_of_month = (days_in_month[month] + current_first_day_of_month) % 7
+			current_first_day_of_month = ((days_in_month[month] + current_first_day_of_month) % 7)
+			// fmt.Println("The first day of the", month, "th month of",
+			// year, "is", weekday_of[current_first_day_of_month],
+			// days_in_month[month], current_first_day_of_month)
 			if current_first_day_of_month == 0 {
 				number_of_first_month_Sundays++
-				fmt.Println("The first day of the", month, "th month of",
-					year, "is", weekday_of[current_first_day_of_month])
-				fmt.Println("current Sunday counter:", number_of_first_month_Sundays)
+				// fmt.Println("current Sunday counter:", number_of_first_month_Sundays)
 			}
 		}
 	}
