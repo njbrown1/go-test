e8e508c21e8e1ceca9e24e4a7905532f184afe55
Nathan Brown <nathanjeffreybrown@gmail.com>
2023-06-17 08:31:06 -0600

    I think the algorithm finally works now, but I'm in the mountains and have no service, so I can't check my answer yet

--- Problems 011-020/proj_euler_p019.go
+++ Problems 011-020/proj_euler_p019.go
@@ -5,13 +5,13 @@ import "fmt"
 func main() {
 
 	number_of_first_month_Sundays := 0
-	current_first_day_of_month := 5 // December 1st, 1900 was a Saturday
-	days_in_month := map[int]int{1: 31, 3: 31, 4: 30, 5: 31,
-		6: 30, 7: 31, 8: 31, 9: 30, 10: 31, 11: 30, 12: 31} // don't assign a number of days to February – yet
+	current_first_day_of_month := 6 // December 1st, 1900 was a Saturday
+	days_in_month := map[int]int{0: 31, 1: 31, 3: 31, 4: 30, 5: 31,
+		6: 30, 7: 31, 8: 31, 9: 30, 10: 31, 11: 30} // don't assign a number of days to February – yet
 	weekday_of := map[int]string{0: "Sun", 1: "Mon", 2: "Tue",
 		3: "Wed", 4: "Thu", 5: "Fri", 6: "Sat"}
 
-	for year := 1901; year <= 2000; year++ { // probably inclusive, but idk
+	for year := 1901; year <= 2000; year++ { // probably inclusive
 
 		// determine whether a year is a leap year, and set February to that many days
 		if (year%4 == 0) && (year%100 != 0) {
@@ -24,15 +24,19 @@ func main() {
 
 		for month := 1; month <= 12; month++ {
 			current_first_day_of_month = ((days_in_month[month-1] + current_first_day_of_month) % 7)
+
 			fmt.Println("The first day of the", month, "th month of",
 				year, "is", weekday_of[current_first_day_of_month], "|",
-				days_in_month[month], current_first_day_of_month)
+				"number of days:", days_in_month[month],
+				"| number equivalent of day:", current_first_day_of_month) // debugging output
+
 			if current_first_day_of_month == 0 {
 				number_of_first_month_Sundays++
-				// fmt.Println("current Sunday counter:", number_of_first_month_Sundays)
+				fmt.Println("current Sunday counter:", number_of_first_month_Sundays)
 			}
 		}
 	}
+	fmt.Println(number_of_first_month_Sundays)
 }
 
 // You are given the following information, but you may prefer to do some research for yourself.
