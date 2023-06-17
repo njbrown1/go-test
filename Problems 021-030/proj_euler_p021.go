ef1e146f37a2c65a881b92efb5a377e874e2e06e
Nathan Brown <nathanjeffreybrown@gmail.com>
2023-06-17 15:01:55 -0600

    p#021 solved

--- Problems 021-030/proj_euler_p021.go
+++ Problems 021-030/proj_euler_p021.go
@@ -14,26 +14,12 @@ func is_k_contained_in_amicable_numbers(amicable_numbers []int, number int) bool
 	return false
 }
 
-func main() {
-	var amicable_numbers []int
-	fmt.Println("hi")
-
-	for number := 1; number <= 10; number++ {
-		if is_k_contained_in_amicable_numbers(amicable_numbers, number) == true {
-			// don't bother checking, because it's already added
-		} else {
-			a := find_sum_of_divisors_of_n(number)
-			fmt.Println(a, number)
-		}
-	}
-}
-
 func find_sum_of_divisors_of_n(n int) int {
 
-	var factors_slice []int                                    // initialize an empty slice that factors will be added to
+	factors_slice := []int{1}                                  // initialize an empty slice that factors will be added to
 	square_root_of_n := int(math.Floor(math.Sqrt(float64(n)))) // finds square root of n, rounded DOWN
 
-	for i := 1; i <= square_root_of_n; i++ {
+	for i := 2; i <= square_root_of_n; i++ {
 		if n%i == 0 {
 			if float64(i) == math.Sqrt(float64(n)) { // if i is the square root of n...
 				factors_slice = append(factors_slice, i) // append the factor only ONCE
@@ -47,6 +33,26 @@ func find_sum_of_divisors_of_n(n int) int {
 		sum_of_factors += factors_slice[i]
 	}
 	return sum_of_factors
+}
+
+func main() {
+	var amicable_numbers []int
+	fmt.Println("hi")
+
+	for number := 1; number <= 10000; number++ {
+		if is_k_contained_in_amicable_numbers(amicable_numbers, number) == false {
+			a := find_sum_of_divisors_of_n(number)
+			if number == find_sum_of_divisors_of_n(a) && number != a {
+				amicable_numbers = append(amicable_numbers, a, number)
+			}
+		}
+	}
+	sum_of_amicable_numbers := 0
+	for i := range amicable_numbers {
+		sum_of_amicable_numbers += amicable_numbers[i]
+	}
+	fmt.Println(amicable_numbers)
+	fmt.Println(sum_of_amicable_numbers)
 }
 
 // evaluate the sum of all the amicable numbers under 10000.
