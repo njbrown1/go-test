5bcb4329998b4a919957994a3b62fd14a5844a4b
Nathan Brown <nathanjeffreybrown@gmail.com>
2023-06-23 14:03:02 -0500

    find_sum_of_letters_in_name is up and running correctly

--- Problems 021-030/proj_euler_p022.go
+++ Problems 021-030/proj_euler_p022.go
@@ -5,15 +5,13 @@ import "regexp"
 import "os"
 
 func main() {
-	fmt.Println("hello world")
-
 	input_names, _ := os.ReadFile("name_testing.txt")
 	cleaned_names := extract_names(string(input_names))
 	fmt.Println(cleaned_names)
-
+	fmt.Println(find_sum_of_letters_in_name("COLIN"))
 }
 
-func extract_names(text string) []string {
+func extract_names(text string) []string { // takes the input names and returns a slice with all of the 'cleaned' names
 	re := regexp.MustCompile(`\s*"([^"]+)"\s*`)
 	matches := re.FindAllStringSubmatch(text, -1) // -1 finds all matches, apparently
 
@@ -23,4 +21,20 @@ func extract_names(text string) []string {
 		names = append(names, name)
 	}
 	return names
+}
+
+func find_sum_of_letters_in_name(name string) int {
+	var alphabet string = ":ABCDEFGHIJKLMNOPQRSTUVWXYZ" // ':' is just an arbitrary start character
+	sum_of_letters := 0
+
+	for i := range name {
+		nth_letter := name[i]
+		for letter := 1; letter <= 26; letter++ {
+			if alphabet[letter] == nth_letter {
+				sum_of_letters += letter
+				break
+			}
+		}
+	}
+	return sum_of_letters
 }
