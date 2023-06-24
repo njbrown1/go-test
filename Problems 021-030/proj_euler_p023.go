1c9afbc19f79e8f905d6a54821de35680e89ed29
Nathan Brown <nathanjeffreybrown@gmail.com>
2023-06-24 16:42:31 -0500

    have some code I want to save, but now I'll try something different

--- Problems 021-030/proj_euler_p023.go
+++ Problems 021-030/proj_euler_p023.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"math"
+	"sort"
 )
 
 func find_sum_of_divisors_of_n(n int) int {
@@ -28,7 +29,7 @@ func find_sum_of_divisors_of_n(n int) int {
 
 func main() {
 	abundant_numbers := []int{12}
-	for number := 13; number <= 28123; number++ {
+	for number := 13; number <= 1000; number++ { // max: 28123
 		sum_of_factors := find_sum_of_divisors_of_n(number)
 		if sum_of_factors > number {
 			abundant_numbers = append(abundant_numbers, number)
@@ -36,4 +37,15 @@ func main() {
 	}
 	fmt.Println(abundant_numbers)
 	fmt.Println(len(abundant_numbers))
+	integer := 116
+
+	for _, abundant_number := range abundant_numbers {
+		reduced_integer := integer - abundant_number
+		fmt.Println(reduced_integer, integer, abundant_number)
+		index := sort.SearchInts(abundant_numbers, reduced_integer)
+		if index < len(abundant_numbers) && abundant_numbers[index] == integer {
+			fmt.Println(integer, "is the sum of", abundant_number, "and", abundant_numbers[index])
+			break
+		}
+	}
 }
