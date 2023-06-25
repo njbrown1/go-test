3bee394170b2cff185c840bbf36f93cb51f42c41
Nathan Brown <nathanjeffreybrown@gmail.com>
2023-06-24 21:37:18 -0500

    added removeelement function

--- Problems 021-030/proj_euler_p024.go
+++ Problems 021-030/proj_euler_p024.go
@@ -1,7 +1,9 @@
 package main
 
-import "fmt"
-import "math"
+import (
+	"fmt"
+	"math"
+)
 
 func factorial_of(number int) int {
 	multiplication_total := 1
@@ -11,20 +13,27 @@ func factorial_of(number int) int {
 	return multiplication_total
 }
 
+func removeElement(slice []int, index int) []int {
+	return append(slice[:index], slice[index+1:]...)
+}
+
 func main() {
+
 	fmt.Println("hi")
+	fmt.Println("factorial of 4:", factorial_of(1))
 	// 10! = 3628800
 	// "0123456789"
-
 	// xth_lexico_permutation := 4
+	// subtract 1 to perm to make work?
+	// small_digits_slice := []int{0, 1, 2, 3}
 
-	whats_left := 1000000
+	digits_slice := []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}
 
-	first_digit := math.Floor(float64(whats_left / factorial_of(9)))
-	whats_left -= int(first_digit) * factorial_of(9)
-	fmt.Println(whats_left, first_digit, factorial_of(9))
+	result := removeElement(digits_slice, 4)
+	fmt.Println(result)
 
-	second_digit := math.Floor(float64(whats_left / factorial_of(8)))
-	whats_left -= int(second_digit) * factorial_of(8)
-	fmt.Println(whats_left, second_digit, factorial_of(8))
+	position := 1
+	first_digit := math.Floor(float64(position-1) / float64(factorial_of(3)))
+	fmt.Println(first_digit)
+
 }
