2829de3d49723447a08626fc66f990815b10804f
Nathan Brown <nathanjeffreybrown@gmail.com>
2023-06-25 19:02:30 -0500

    solved p#025. bigInt is overpowered

--- Problems 021-030/proj_euler_p025.go
+++ Problems 021-030/proj_euler_p025.go
@@ -8,7 +8,7 @@ func main() {
 	b := big.NewInt(1)
 	storage := big.NewInt(1)
 
-	for i := 1; i <= 10; i++ {
+	for i := 1; len(b.String()) < 1000; i++ {
 		storage.Add(a, b)
 		a.Set(b)
 		b.Set(storage)
