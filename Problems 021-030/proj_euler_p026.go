7e911b278a1e051d19ad87673af8f25a0101eabc
Nathan Brown <nathanjeffreybrown@gmail.com>
2023-06-27 20:20:23 -0500

    sike my function doesn't work for d > 100. fixing now

--- Problems 021-030/proj_euler_p026.go
+++ Problems 021-030/proj_euler_p026.go
@@ -6,22 +6,23 @@ import "math"
 func perform_long_divison(divisor int, decimal_places int) string { // where the numerator HAS to be 1, and the divisor is d (as specified in p#026).
 	remainder := 1
 	quotient := "0."
-	for i := 1; i <= 99; i++ {
+	for i := 1; i <= decimal_places; i++ {
 		if remainder == 0 {
 			break
 		}
 		for divisor > remainder {
 			remainder *= 10
 		}
-		digit_or_digits := math.Floor(float64(remainder) / float64(divisor))
-		quotient += fmt.Sprint(digit_or_digits)
-		remainder -= int(digit_or_digits) * divisor
+		digit := math.Floor(float64(remainder) / float64(divisor))
+		quotient += fmt.Sprint(digit)
+		fmt.Println(remainder, digit, quotient)
+		remainder -= int(digit) * divisor
 	}
 	return quotient
 }
 
 func main() {
-	d := 9
+	d := 260
 	result := perform_long_divison(d, 30)
 	fmt.Println("1 divided by", d, "is approximately:", result)
 }
