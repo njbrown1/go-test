7b2ef5e75e826ad5e27262c2004dae42e61aef93
Nathan Brown <nathanjeffreybrown@gmail.com>
2023-07-01 11:13:20 -0500

    I THINK IT WORKS LETSSSSSS GOOOOOOOOOO

--- Problems 021-030/proj_euler_p026.go
+++ Problems 021-030/proj_euler_p026.go
@@ -9,12 +9,15 @@ func perform_long_divison(divisor int, decimal_places int) (string, int) { // wh
 	remainder_list := []int{}
 	remainder := 1
 	quotient := "0."
-	length_of_recurring_sequence := 0
+	is_terminating := false
 
 	for i := 1; i <= decimal_places; i++ {
+
 		if remainder == 0 {
+			is_terminating = true
 			break
 		}
+
 		number_of_multiplications := 0
 		for divisor > remainder {
 			remainder *= 10
@@ -24,25 +27,34 @@ func perform_long_divison(divisor int, decimal_places int) (string, int) { // wh
 				quotient += "0"
 			}
 		}
+
 		digit := math.Floor(float64(remainder) / float64(divisor))
 		quotient += fmt.Sprint(digit)
-
 		fmt.Println(remainder, digit, quotient, remainder_list)
-
 		remainder -= int(digit) * divisor
-		for i := range remainder_list {
-			fmt.Println(i, remainder_list)
-			if remainder == remainder_list[i] { // if the last element in remainder_list = an entry in remainder_list
-				fmt.Println("if statement does run")
-				length_of_recurring_sequence = (len(remainder_list) - 1) - i
-			}
-		}
+
 	}
+
+	// check to find the longest recurring sequence
+	length_of_recurring_sequence := 0
+	if is_terminating == false {
+		last_index := len(remainder_list) - 1
+		very_last_remainder := remainder_list[last_index]
+		fmt.Println(very_last_remainder, last_index)
+		i := last_index - 1
+		fmt.Println(very_last_remainder, i, last_index)
+		for very_last_remainder != remainder_list[i] {
+			i--
+		}
+		fmt.Println(last_index, i)
+		length_of_recurring_sequence = last_index - i
+	}
+
 	return quotient, length_of_recurring_sequence
 }
 
 func main() {
-	d := 70
+	d := 9
 	quotient, length_of_recurring_sequence := perform_long_divison(d, 20)
 	fmt.Println(quotient, length_of_recurring_sequence)
 }
