d7ed95020ee27bd84a34f93edd131160039001e8
Nathan Brown <nathanjeffreybrown@gmail.com>
2023-07-01 11:19:38 -0500

    solved it barely lol

--- Problems 021-030/proj_euler_p026.go
+++ Problems 021-030/proj_euler_p026.go
@@ -30,23 +30,20 @@ func perform_long_divison(divisor int, decimal_places int) (string, int) { // wh
 
 		digit := math.Floor(float64(remainder) / float64(divisor))
 		quotient += fmt.Sprint(digit)
-		fmt.Println(remainder, digit, quotient, remainder_list)
 		remainder -= int(digit) * divisor
 
 	}
 
 	// check to find the longest recurring sequence
+
 	length_of_recurring_sequence := 0
-	if is_terminating == false {
+	if is_terminating == false { // only check if the number is non-terminating
 		last_index := len(remainder_list) - 1
 		very_last_remainder := remainder_list[last_index]
-		fmt.Println(very_last_remainder, last_index)
 		i := last_index - 1
-		fmt.Println(very_last_remainder, i, last_index)
 		for very_last_remainder != remainder_list[i] {
 			i--
 		}
-		fmt.Println(last_index, i)
 		length_of_recurring_sequence = last_index - i
 	}
 
@@ -54,7 +51,14 @@ func perform_long_divison(divisor int, decimal_places int) (string, int) { // wh
 }
 
 func main() {
-	d := 9
-	quotient, length_of_recurring_sequence := perform_long_divison(d, 20)
-	fmt.Println(quotient, length_of_recurring_sequence)
+
+	longest_length_of_recurring_sequence := 0
+	for d := 1; d <= 1000; d++ {
+		quotient, length_of_recurring_sequence := perform_long_divison(d, 10000)
+		if length_of_recurring_sequence > longest_length_of_recurring_sequence {
+			fmt.Println("d", d, "quotient", quotient, "length", length_of_recurring_sequence)
+			longest_length_of_recurring_sequence = length_of_recurring_sequence
+		}
+	}
+	fmt.Println(longest_length_of_recurring_sequence)
 }
