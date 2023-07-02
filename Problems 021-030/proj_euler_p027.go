7107234308e103980d87106e1b0444c961ac4f37
Nathan Brown <nathanjeffreybrown@gmail.com>
2023-07-02 16:14:46 -0500

    created is_n_prime function

--- Problems 021-030/proj_euler_p027.go
+++ Problems 021-030/proj_euler_p027.go
@@ -5,14 +5,13 @@ import "fmt"
 func generate_primes_v027(limit int) []int { // returns a slice with prime numbers from 2 - limit.
 	prime_slice := []int{2, 3, 5}
 	for i := 4; i <= limit; i++ { // for finding prime numbers up to 'limit'.
-
-		is_k_prime := true
+		is_i_prime := true
 		for _, prime := range prime_slice {
 			if i%prime == 0 {
-				is_k_prime = false
+				is_i_prime = false
 			}
 		}
-		if is_k_prime == true {
+		if is_i_prime == true {
 			prime_slice = append(prime_slice, i) // if k IS prime, add it to prime_slice.
 		}
 	}
@@ -23,7 +22,7 @@ func main() {
 	fmt.Println("hi")
 	// a and b must both be odd
 
-	fmt.Println(generate_primes_v027(17))
+	fmt.Println(generate_primes_v027(100))
 
 	for a := 0; a <= 1; a++ {
 		for b := 0; b <= 1; b++ {
