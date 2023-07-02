e98fc70802801dede30531cfa4b75a2ea488dab8
Nathan Brown <nathanjeffreybrown@gmail.com>
2023-07-02 17:11:42 -0500

    solved p#028

--- Problems 021-030/proj_euler_p028.go
+++ Problems 021-030/proj_euler_p028.go
@@ -4,18 +4,16 @@ import "fmt"
 
 func main() {
 
-	spiral_side_length := 5
+	spiral_side_length := 1001
 	increment := 2
-	sum_of_diagonals := 0
+	sum_of_diagonals := 1
 
-	for n := 1; n <= (spiral_side_length * spiral_side_length); {
-		for k := 1; k <= 4; k++ {
+	for n := 1; n < (spiral_side_length * spiral_side_length); {
+		for k := 1; k <= 4; k++ { // k has no significance here, I just want it to repeat 4 times
 			n += increment
 			sum_of_diagonals += n
-			fmt.Println("after:", sum_of_diagonals, n, increment)
 		}
 		increment += 2
 	}
-
 	fmt.Println(sum_of_diagonals)
 }
