7932faa39e11cca5da563df48d92c34046335e95
Nathan Brown <nathanjeffreybrown@gmail.com>
2023-07-02 16:46:20 -0500

    think I'm on the right track here

--- Problems 021-030/proj_euler_p027.go
+++ Problems 021-030/proj_euler_p027.go
@@ -19,13 +19,27 @@ func is_k_prime(k int) bool {
 }
 
 func main() {
-
-	result := is_k_prime(1009)
-	fmt.Println(result)
-
-	for a := 0; a <= 1; a++ {
-		for b := 0; b <= 1; b++ {
-			fmt.Println(a, b)
+	current_best_number_of_consecutive_primes := 0
+	for a := 0; a <= 3; a++ {
+		for b := 0; b <= 3; b++ {
+			n := 0
+			number_of_consecutive_primes := 0
+			for never_stop := true; never_stop == true; { // this 'for' loop will only stop once it's broken
+				evaluation := (n * n) + (a * n) + b
+				if is_k_prime(evaluation) == false {
+					if number_of_consecutive_primes > current_best_number_of_consecutive_primes {
+						current_best_number_of_consecutive_primes = number_of_consecutive_primes
+						fmt.Println("ALERT: new best a:", a, "and b:", b, "with a record of", number_of_consecutive_primes,
+							"number of consecutive primes! the product of a and b is:", (a * b))
+						break
+					} else {
+						break
+					}
+				} else {
+					n++
+					number_of_consecutive_primes++
+				}
+			}
 		}
 	}
 }
