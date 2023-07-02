15af0e4abae73f2570657dbd9f399a0b199d81bc
Nathan Brown <nathanjeffreybrown@gmail.com>
2023-07-02 16:28:41 -0500

    made way better function – is_k_prime – that is way simpler and works for all of the cases I tested

--- Problems 021-030/proj_euler_p027.go
+++ Problems 021-030/proj_euler_p027.go
@@ -5,27 +5,13 @@ import (
 	"math"
 )
 
-func generate_primes_v027(limit int) []int { // returns a slice with prime numbers from 2 - limit.
-	prime_slice := []int{2, 3, 5}
-	for i := 4; i <= limit; i++ { // for finding prime numbers up to 'limit'.
-		is_i_prime := true
-		for _, prime := range prime_slice {
-			if i%prime == 0 {
-				is_i_prime = false
-			}
-		}
-		if is_i_prime == true {
-			prime_slice = append(prime_slice, i) // if k IS prime, add it to prime_slice.
-		}
+func is_k_prime(k int) bool {
+	if k < 2 {
+		return false
 	}
-	return prime_slice
-}
-
-func is_n_prime(n int) bool {
-	square_root_of_n := math.Ceil(math.Sqrt(float64(n)))
-	prime_slice := generate_primes_v027(int(square_root_of_n))
-	for prime := range prime_slice {
-		if n%prime == 0 {
+	square_root_of_k := math.Ceil(math.Sqrt(float64(k)))
+	for possible_divisor := 2; possible_divisor <= int(square_root_of_k); possible_divisor++ {
+		if k%possible_divisor == 0 {
 			return false
 		}
 	}
@@ -34,8 +20,8 @@ func is_n_prime(n int) bool {
 
 func main() {
 
-	evaluation := 42
-	fmt.Println(generate_primes_v027(100))
+	result := is_k_prime(1009)
+	fmt.Println(result)
 
 	for a := 0; a <= 1; a++ {
 		for b := 0; b <= 1; b++ {
