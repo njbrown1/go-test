56aa79d650f7a213e8c22bc261290344bce7d116
Nathan Brown <nathanjeffreybrown@gmail.com>
2023-07-02 16:49:50 -0500

    added code comments. problem solved

--- Problems 021-030/proj_euler_p027.go
+++ Problems 021-030/proj_euler_p027.go
@@ -5,11 +5,11 @@ import (
 	"math"
 )
 
-func is_k_prime(k int) bool {
+func is_k_prime(k int) bool { // returns 'true' if the number is prime, and 'false' if the number is not prime.
 	if k < 2 {
 		return false
 	}
-	square_root_of_k := math.Ceil(math.Sqrt(float64(k)))
+	square_root_of_k := math.Ceil(math.Sqrt(float64(k))) // you only have to check up to the square root, which is great for computational time.
 	for possible_divisor := 2; possible_divisor <= int(square_root_of_k); possible_divisor++ {
 		if k%possible_divisor == 0 {
 			return false
