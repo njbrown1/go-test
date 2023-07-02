9e9f5bf8f9ae726a098c998c5a1e7b79464b8d7c
Nathan Brown <nathanjeffreybrown@gmail.com>
2023-07-02 17:12:41 -0500

    adjusted code

--- Problems 021-030/proj_euler_p028.go
+++ Problems 021-030/proj_euler_p028.go
@@ -3,11 +3,9 @@ package main
 import "fmt"
 
 func main() {
-
 	spiral_side_length := 1001
 	increment := 2
 	sum_of_diagonals := 1
-
 	for n := 1; n < (spiral_side_length * spiral_side_length); {
 		for k := 1; k <= 4; k++ { // k has no significance here, I just want it to repeat 4 times
 			n += increment
