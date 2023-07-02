904978d5e34e55f4f115093934a87380aa017a0d
Nathan Brown <nathanjeffreybrown@gmail.com>
2023-07-02 18:22:16 -0500

    solved p#030

--- Problems 021-030/proj_euler_p030.go
+++ Problems 021-030/proj_euler_p030.go
@@ -7,19 +7,23 @@ import (
 
 func main() {
 	successful_numbers := []int{} // all numbers that satisfy the condition set in p#030
-	for number := 2; number <= 99999; number++ {
+	for number := 2; number <= 999999; number++ {
 		sum_of_fifth_powers := 0
 		k := number // so we don't mess with the wider 'for' loop
 		for k > 0 {
 			digit := (k % 10)
 			k -= digit
 			k /= 10
-			sum_of_fifth_powers += int(math.Pow(float64(digit), 4))
+			sum_of_fifth_powers += int(math.Pow(float64(digit), 5))
 		}
 		if number == sum_of_fifth_powers {
 			successful_numbers = append(successful_numbers, number)
 			fmt.Println(successful_numbers)
 		}
 	}
-	fmt.Println(successful_numbers)
+	sum_of_successful_numbers := 0
+	for _, successful_number := range successful_numbers {
+		sum_of_successful_numbers += successful_number
+	}
+	fmt.Println(successful_numbers, "sum:", sum_of_successful_numbers)
 }
