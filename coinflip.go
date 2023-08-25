0543e0d8162eaf74e46b121bccce0d8a2c2b5697
Nathan Brown <nathanjeffreybrown@gmail.com>
2023-08-25 09:10:59 -0500

    flip a coin 100 times simulation working

--- coinflip.go
+++ coinflip.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+)
 
 func main() {
-	fmt.Println("hello world")
+	total := 0
+	for k := 0; k <= 100; k++ {
+		flip := rand.Intn(2) // will psuedorandomly generate either a 0 or a 1. 0 = tails, and 1 = heads
+		adjust_total := -1   // set adjust_total_by as a default -1 (tails)
+		if flip == 1 {
+			adjust_total = 1
+		}
+		fmt.Print(flip)
+		total += adjust_total
+	}
+	fmt.Println("")
+	fmt.Println("")
+	fmt.Println(total)
 }
