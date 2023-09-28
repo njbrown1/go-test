456e56fd513338691c7901883b7b153190ca535e
Nathan Brown <nathanjeffreybrown@gmail.com>
2023-09-28 15:32:03 -0500

    started constructing class structure and figured out that it works like I would expect with Java

--- pseudorandom.go
+++ pseudorandom.go
@@ -5,7 +5,25 @@ import (
 	"math/rand"
 )
 
+type TheRock struct {
+	myScore int
+}
+
+func (m *TheRock) GainOnePoint() {
+	m.myScore++
+}
+
+func addNumbers(x int, y int) int {
+	return (x + y)
+}
+
 func main() {
 	fmt.Println("hello world")
 	fmt.Println(rand.Intn(25))
+	theRockOne := TheRock{4}
+	fmt.Println(theRockOne)
+	theRockOne.GainOnePoint()
+	fmt.Println(theRockOne)
+	theRockOne.myScore = addNumbers(3, 7)
+	fmt.Println(theRockOne)
 }
