93abb1b8dc1fb928a7e56661dd09229f99b2abe1
Nathan Brown <nathanjeffreybrown@gmail.com>
2023-09-30 11:14:49 -0500

    I'm not doing anything, but allPlayers forgets the number it chose previously. this is incredibly frustrating

--- pseudorandom.go
+++ pseudorandom.go
@@ -9,13 +9,18 @@ import (
 type Player struct {
 	myScore                     int
 	numbersIHaveChosenInThePast []int
-	numbersChosenLastRound      []int
 	myCurrentlyChosenNumber     int
 	behavior                    string
 }
 
-func GenerateRandomNumberFromOneToNInclusive(n int) int {
+func generateRandomNumberFromOneToNInclusive(n int) int {
 	return (rand.Intn(n) + 1)
+}
+
+func chooseRandomNumberFromSlice(numbers []int) int {
+	rand.Seed(time.Now().UnixNano()) // choose a random seed based on the time
+	randomIndex := rand.Intn(len(numbers))
+	return numbers[randomIndex]
 }
 
 func findMostCommonNumbers(nums []int) []int {
@@ -61,46 +66,55 @@ func findLeastCommonNumbers(nums []int) []int {
 	return leastCommonNumbers
 }
 
-func (p *Player) ChooseNumber(numbersChosenLastRound []int) {
-	p.numbersChosenLastRound = numbersChosenLastRound
+func (p *Player) chooseNumber(numbersChosenLastRound []int) {
 	switch p.behavior {
 	case "rock":
 		if len(p.numbersIHaveChosenInThePast) == 0 {
-			p.myCurrentlyChosenNumber = GenerateRandomNumberFromOneToNInclusive(10)
+			p.myCurrentlyChosenNumber = generateRandomNumberFromOneToNInclusive(10)
 		}
 	case "opportunist":
-		leastChosenNumbers := findLeastCommonNumbers(p.numbersChosenLastRound)
+		leastChosenNumbers := findLeastCommonNumbers(numbersChosenLastRound)
 		p.myCurrentlyChosenNumber = chooseRandomNumberFromSlice(leastChosenNumbers)
 	case "realEstateAgent":
-		mostChosenNumbers := findMostCommonNumbers(p.numbersChosenLastRound)
+		mostChosenNumbers := findMostCommonNumbers(numbersChosenLastRound)
 		p.myCurrentlyChosenNumber = chooseRandomNumberFromSlice(mostChosenNumbers)
 	case "completelyRandom":
-		p.myCurrentlyChosenNumber = GenerateRandomNumberFromOneToNInclusive(10)
+		p.myCurrentlyChosenNumber = generateRandomNumberFromOneToNInclusive(10)
 	}
 	p.numbersIHaveChosenInThePast = append(p.numbersIHaveChosenInThePast, p.myCurrentlyChosenNumber) // add the number chosen to the history
 }
 
-func chooseRandomNumberFromSlice(numbers []int) int {
-	rand.Seed(time.Now().UnixNano()) // choose a random seed based on the time
-	randomIndex := rand.Intn(len(numbers))
-	return numbers[randomIndex]
+func (p *Player) UpdateScore(numbersChosenThisRound []int) {
+	counter := 0
+	fmt.Println(p.myCurrentlyChosenNumber)
+	for number := range numbersChosenThisRound {
+		if p.myCurrentlyChosenNumber == number {
+			counter++
+			fmt.Println(number, counter)
+		}
+	}
+	if counter == 1 { // if the number only occurs once in the numbersChosenThisRound (ie. the player chose a unique number)
+		p.myScore++
+	}
 }
 
 func main() {
 	numbersChosenLastRound := []int{}
+	numbersChosenThisRound := []int{}
 	allPlayers := []Player{
-		{0, []int{}, []int{}, 0, "completelyRandom"},
-		{0, []int{}, []int{}, 0, "rock"},
+		{0, []int{}, 0, "completelyRandom"},
+		{0, []int{}, 0, "rock"},
 	}
 	for _, player := range allPlayers {
-		player.ChooseNumber(numbersChosenLastRound)
+		player.chooseNumber(numbersChosenLastRound)
 		fmt.Println(player, "first")
-	}
-	numbersChosenLastRound = []int{} // clear the slice
-	for _, player := range allPlayers {
-		fmt.Println(player)
-		numbersChosenLastRound = append(numbersChosenLastRound, player.myCurrentlyChosenNumber)
-		fmt.Println(player)
+		numbersChosenThisRound = append(numbersChosenThisRound, player.myCurrentlyChosenNumber)
 	}
 	fmt.Println(numbersChosenLastRound)
+	fmt.Println(numbersChosenThisRound)
+	fmt.Println(allPlayers, "after numbersChosenThisRound")
+	for _, player := range allPlayers {
+		player.UpdateScore(numbersChosenThisRound)
+	}
+	fmt.Println(allPlayers)
 }
